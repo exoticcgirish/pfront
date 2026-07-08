ba13e59ec2af8548e154b26ccf4998d72e01c438
phoniccgirish <gy63862829@gmail.com>
2026-07-08 12:45:00 +0530

    link fix

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,23 +2,23 @@ import React from "react";
 import { Linkedin, Github } from "lucide-react";
 
 const Footer = () => (
-  <footer className='bg-white mt-20 border-t border-gray-200 py-10 text-center'>
-    <div className='flex justify-center space-x-6 mb-4'>
+  <footer className='py-10 mt-20 text-center bg-white border-t border-gray-200'>
+    <div className='flex justify-center mb-4 space-x-6'>
       <a
-        href='https://www.linkedin.com/in/girish-yadav'
+        href='https://www.linkedin.com/in/girishyadav1/'
         target='_blank'
         rel='noopener noreferrer'
         className='text-gray-500 hover:text-blue-600'
       >
-        <Linkedin className='h-6 w-6' />
+        <Linkedin className='w-6 h-6' />
       </a>
       <a
-        href='https://github.com/'
+        href='https://github.com/exoticcgirish'
         target='_blank'
         rel='noopener noreferrer'
         className='text-gray-500 hover:text-blue-600'
       >
-        <Github className='h-6 w-6' />
+        <Github className='w-6 h-6' />
       </a>
     </div>
     <p className='text-sm text-gray-500'>
